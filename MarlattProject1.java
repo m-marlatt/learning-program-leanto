c48c63f9300f40e280aee58c283da0d3fc1b228c
m-marlatt <55703582+m-marlatt@users.noreply.github.com>
2019-09-26 14:54:23 -0400

    Add files via upload

---
+++ MarlattProject1.java
@@ -0,0 +1,20 @@
+
+public class Lines {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}		//Something stupid i learned is that some gamecubes do not like off brand
+		// Power adapters. It makes them have a video display problem. 
+		// How does this even link I guess that makes sense if they are not getting
+		// the same amount of voltage but they are. Does Nintendo give their equipment
+		// the ability to determine between a real Nintendo product and an off-brand
+		// product? Honestly seems like something Apple would do instead. Cook you greedy
+		// demon. 
+		// Did you know that the funko pops have recently had a severe drop in value
+		// They are officially becoming the new beanie babies and that is just a shame.
+		// Mostly though because I have 30 and like 20 were bought due to the fact that their
+		// value was supposed to go up. Now they are worth less than I bought them for 
+		// *SAD TROMBONE SOUNDS*
