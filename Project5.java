bd32c84470420c666c154d492891e760d06d6254
m-marlatt <55703582+m-marlatt@users.noreply.github.com>
2019-12-06 09:26:55 -0500

    Add files via upload

---
+++ Project5.java
@@ -0,0 +1,52 @@
+import java.util.Scanner;
+import java.util.Random;
+
+		// Project: Project 5
+		// Author: Max Marlatt
+		// Github: m-marlatt
+		// Description: Methods
+		// Date: 11/5/19
+		// Version One
+
+
+public class Project5 {
+
+	public static int main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int value1 = 0;
+		int value2 = 0;
+		
+
+		//Methods
+		Scanner keyboard = new Scanner(System.in);
+		Random rnd = new Random(); 
+		total = sum(value1, value2);
+
+		int diff(int num1, int num2)
+		{int result;
+		result = num2 - num1;
+		return result;
+		}
+		int sum(int num1, int num2)
+		{int result;
+		result = num1 + num2;
+		return result;
+		}
+
+
+
+
+
+
+
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
